b6eabcff62f873003d142fa2b8fd507f4ddfceb6
Arshad2502 <arshad.jhs25@gmail.com>
2025-08-21 02:17:31 +0530

    Create variables.tf for budgetalert

---
+++ budgetalertvmss/variables.tf
@@ -0,0 +1,9 @@
+variable "prefix" {
+  description = "The prefix which should be used for all resources"
+  default  = "ars"
+}
+
+variable "location" {
+  description = "The Azure Region in which all resources should be created."
+  default = "Australia East"
+}
