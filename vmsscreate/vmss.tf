a7a503da04cefa50e89617102cacb5d18f03d524
Arshad2502 <arshad.jhs25@gmail.com>
2025-08-21 03:03:08 +0530

    Update vmss.tf

--- vmsscreate/vmss.tf
+++ vmsscreate/vmss.tf
@@ -32,6 +32,7 @@ resource "azurerm_subnet" "internal" {
 }
 
 resource "azurerm_linux_virtual_machine_scale_set" "main" {
+  depends_on = [azurerm_subnet.internal]
   name                            = "${var.prefix}-vmss"
   resource_group_name             = azurerm_resource_group.main.name
   location                        = azurerm_resource_group.main.location
