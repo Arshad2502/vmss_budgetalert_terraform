e46034fcaa35e721c10b4eda213d7f8db9c837c6
Arshad2502 <arshad.jhs25@gmail.com>
2025-08-21 02:16:11 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,10 +1,10 @@
 variable "prefix" {
-  description = "The prefix which should be used for all resources in this example"
+  description = "The prefix which should be used for all resources"
   default  = "ars"
 }
 
 variable "location" {
-  description = "The Azure Region in which all resources in this example should be created."
+  description = "The Azure Region in which all resources should be created."
   default = "Australia East"
 }
 
