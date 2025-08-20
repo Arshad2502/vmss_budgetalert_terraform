113a4bbd023e2c6f0d68192a574b9bb627e0aae1
Arshad2502 <arshad.jhs25@gmail.com>
2025-08-21 02:22:39 +0530

    Update vmss.tf

--- vmsscreate/vmss.tf
+++ vmsscreate/vmss.tf
@@ -1,3 +1,17 @@
+terraform {
+  required_version = ">= 1.6.0"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.110.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
 resource "azurerm_resource_group" "main" {
   name     = "${var.prefix}-resources"
   location = var.location
